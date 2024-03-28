6a0da4a1dc459c27a4f09b34eb51a1cff54acba0
Srilekha <149182862+Srilekhanalabolu@users.noreply.github.com>
2024-03-28 12:50:36 +0530

    mul

---
+++ mul.java
@@ -0,0 +1,11 @@
+class mul{
+public statis void main(String[] args)
+{
+int a=Integer.parseint(args[0])
+System.out.println("mul,");
+for(i=0;i<=10;i++){
+if(i%2!=0){
+System.out.println("mul " +a+ "and " i " is "+(a*i));
+}
+}
+}
