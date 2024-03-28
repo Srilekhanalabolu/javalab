cb91a30aa5554fa738f0fdd7ffc68900aafdbfe8
Srilekha <149182862+Srilekhanalabolu@users.noreply.github.com>
2024-03-28 12:33:39 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,15 @@
+public class array{
+          public static void main(String[] args){
+             int[] arr={1,2,5,6,7,8};
+             comarray co=new comarray();
+             System.out.println("the array3 elements");
+             for(int l:co.arr3){
+                       System.out.println(l);
+             }
+              comarray.main(null);
+             System.out.println("the elements are:");
+             for (int i=0;i<arr.length;i++){
+                  System.out.println(arr[i]);
+            }
+        }
+    }
